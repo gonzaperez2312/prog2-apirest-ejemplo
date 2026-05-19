a5e3a239d1fb8914a39751b2e5b57141ca53ff62
Gonzalo <gonzaperez2312@gmail.com>
2026-05-19 19:15:50 -0300

    Proyecto inicial

---
+++ repoclase/src/main/java/com/apirest/repoclase/RepoclaseApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.repoclase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoclaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoclaseApplication.class, args);
+	}
+
+}
---
+++ repoclase/src/test/java/com/apirest/repoclase/RepoclaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.repoclase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoclaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
